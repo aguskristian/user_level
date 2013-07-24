5cdd3295e3737d981480250ce7c70bcb056a444b
I Made Agus Kristian <aguscristian21@gmail.com>
2013-07-24 17:33:16 +0800

    search

--- application/views/station/tabel_station.php
+++ application/views/station/tabel_station.php
@@ -1,9 +1,37 @@
 <article class="module width_full" style="overflow-x: scroll;">
 		<div class="module_content">
+			
+			
+		
 			<br></br>
 			
 			<?php $this->load->view('header');	?>
 			<?php $this->load->view('main_header_station');?>
+			<div>
+			<strong>
+			
+				<?php 
+				
+					$search = $this->input->post( 'search' );
+					
+					echo $search ? "Menampilkan data pencarian '$search'" : 'Pencarian Data';
+					
+				?>
+				
+			</strong>
+			</div>
+           
+			<table>
+				<tr>
+                <form action="<?php echo site_url() ?>/station/tabel_station" method="post" class="navbar-search pull-left">
+					<tr>
+						<td><input type="search" name="search" class="input-medium search-query" placeholder="search" value="<?php echo $this->input->post( 'search' ) ?>" /></td>
+                    
+						<td><input type="submit" value="Search"></input></td>
+					</tr>
+                </form>
+			</table>
+            
 			<br></br>
     		<table align="center" border="1" cellpadding="0" cellspacing="0" style="font-size:12px" class="table table-bordered">
 
